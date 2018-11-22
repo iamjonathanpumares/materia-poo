85603ca86b70ff44386cd71629cde6e0bea7a02e
Jonathan Pumares <jepc1491@gmail.com>
2018-11-22 14:44:37 -0600

    Se agregaron variables de entorno para la configuración de la base de datos.

--- public/index.php
+++ public/index.php
@@ -24,11 +24,12 @@ use Skynet\Middlewares\AuthRule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
+    'driver'    => getenv('DB_DRIVER'),
     'host'      => getenv('DB_HOST'),
     'database'  => getenv('DB_NAME'),
     'username'  => getenv('DB_USER'),
     'password'  => getenv('DB_PASS'),
+    'port'		=> getenv('DB_PORT'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
